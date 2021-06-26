1ff16684dc3374b3e02177fe5c07be7893db2baf
CameronJYoung <cameronjyoung123@gmail.com>
2021-06-26 18:12:48 +0100

    added url (temporary)

--- src/models/index.js
+++ src/models/index.js
@@ -1,20 +1,24 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/db.config');
 
-const sequelize = new Sequelize(
-	dbConfig.DB,
-	dbConfig.USER,
-	dbConfig.PASSWORD, {
-		host: dbConfig.HOST,
-		dialect: dbConfig.dialect,
-		pool: {
-			max: dbConfig.pool.max,
-			min: dbConfig.pool.min,
-			acquire: dbConfig.pool.acquire,
-			idle: dbConfig.pool.idle,
-		},
-	},
-);
+// const sequelize = new Sequelize(
+// 	dbConfig.DB,
+// 	dbConfig.USER,
+// 	dbConfig.PASSWORD, {
+// 		host: dbConfig.HOST,
+// 		dialect: dbConfig.dialect,
+// 		pool: {
+// 			max: dbConfig.pool.max,
+// 			min: dbConfig.pool.min,
+// 			acquire: dbConfig.pool.acquire,
+// 			idle: dbConfig.pool.idle,
+// 		},
+// 	},
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: dbConfig.dialect
+});
 
 const db = {};
 
