a7ab1f824ddb4baadf29338bf1b4e03f325c22e9
CameronJYoung <cameronjyoung123@gmail.com>
2021-07-10 15:51:46 +0100

    updated cors to work in production

--- server.js
+++ server.js
@@ -17,12 +17,21 @@ app.use(express.urlencoded({
 	extended: true,
 }));
 app.use(express.json());
-app.use(cors({
-	origin: 'http://localhost:3000',
-	credentials: true,
-	methods: 'GET, POST, PUT, PATCH, DELETE',
-	allowedHeaders: 'Content-Type, Accepts, Authorization'
-}));
+if (process.env.NODE_ENV === 'development') {
+	app.use(cors({
+		origin: 'http://localhost:3000',
+		credentials: true,
+		methods: 'GET, POST, PUT, PATCH, DELETE',
+		allowedHeaders: 'Content-Type, Accepts, Authorization'
+	}));
+} else if (process.env.NODE_ENV === 'production') {
+	app.use(cors({
+		origin: 'https://kanbandingo-frontend.herokuapp.com',
+		credentials: true,
+		methods: 'GET, POST, PUT, PATCH, DELETE',
+		allowedHeaders: 'Content-Type, Accepts, Authorization'
+	}));
+}
 
 
 // Initialize DB
