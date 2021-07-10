a4d4c63de7e3894e3d196466984eba237ce08fa9
CameronJYoung <cameronjyoung123@gmail.com>
2021-07-11 00:55:44 +0100

    termporarly changed cors to localhost

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'development') {
 	}));
 } else if (process.env.NODE_ENV === 'production') {
 	app.use(cors({
-		origin: 'https://kanbandingo-frontend.herokuapp.com',
+		origin: 'http://localhost:3000',
 		credentials: true,
 		methods: 'GET, POST, PUT, PATCH, DELETE',
 		allowedHeaders: 'Content-Type, Accepts, Authorization'
