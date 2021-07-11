9ebf764f9b9f5c17f8fb5b37c406d6ee080e4421
CameronJYoung <cameronjyoung123@gmail.com>
2021-07-11 01:24:32 +0100

    logging cookies

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 
 exports.verifyToken = async (req, res, next) => {
 	const token = await req.cookies.token || '';
-
+	console.log(req.cookies);
 	try {
 		if (!token) {
 			return res.status(401).json('You need to Login')
 		}
-		console.log(token);
+		
 		const decrypt = await jwt.verify(token, process.env.JWT_SECRET);
 		req.user = {
 			id: decrypt.id,
